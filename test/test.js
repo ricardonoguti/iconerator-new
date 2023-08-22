dbdaedf334b92b3b07e9e437840da18a77b41ce7
ricardonoguti <ricardonoguti@yahoo.com.br>
2023-08-22 13:29:11 -0300

    support splash and ic notifications

--- test/test.js
+++ test/test.js
@@ -12,23 +12,23 @@ var assert = require('assert'),
     outputPath = path.resolve(__dirname, "./output"),
     timeout = 3000;
 
-describe('iconerator', function(){
-    beforeEach(function(cb){
-        cp.exec("rm -rf " + outputPath, function() {
+describe('iconerator', function () {
+    beforeEach(function (cb) {
+        cp.exec("rm -rf " + outputPath, function () {
             fs.mkdirSync(outputPath);
             cb();
         });
     });
-    afterEach(function(cb){
+    afterEach(function (cb) {
         cb();
     });
-    before(function(cb){
-        fs.mkdir(outputPath, function() {
+    before(function (cb) {
+        fs.mkdir(outputPath, function () {
             cb();
         });
     });
-    after(function(cb){
-        cp.exec("rm -rf " + outputPath, function() {
+    after(function (cb) {
+        cp.exec("rm -rf " + outputPath, function () {
             cb();
         });
     });
@@ -43,9 +43,9 @@ describe('iconerator', function(){
     });
     */
 
-    it('can generate all platform images and proper output directories', function(cb){
+    it('can generate all platform images and proper output directories', function (cb) {
         this.timeout(timeout);
-        iconerator.generateIcons(testImg, testImg2, testImg3, outputPath, null, null, function(err){
+        iconerator.generateIcons(testImg, testImg2, testImg3, outputPath, null, null, null, function (err) {
             assert.ifError(err);
 
             assert(fs.statSync(path.join(outputPath, config.output.iosDir)).isDirectory());
@@ -53,32 +53,32 @@ describe('iconerator', function(){
             assert(fs.statSync(path.join(outputPath, config.output.androidDir)).isDirectory());
 
             var iosIconMeta = _.where(config.icons, { platform: 'iOS' });
-            for(var i = 0; i < iosIconMeta.length; i++){
+            for (var i = 0; i < iosIconMeta.length; i++) {
                 assert(fs.statSync(path.join(outputPath, config.output.iosDir, iosIconMeta[i].file_name)).isFile());
             }
 
             var webIconMeta = _.where(config.icons, { platform: 'Web' });
-            for(var i = 0; i < webIconMeta.length; i++){
+            for (var i = 0; i < webIconMeta.length; i++) {
                 assert(fs.statSync(path.join(outputPath, config.output.webDir, webIconMeta[i].file_name)).isFile());
             }
 
             var androidIconMeta = _.where(config.icons, { platform: 'Android' });
-            for(var i = 0; i < androidIconMeta.length; i++){
+            for (var i = 0; i < androidIconMeta.length; i++) {
                 var meta = androidIconMeta[i];
                 var outputFile;
-                if(meta.type === 'Launcher Icon') 
+                if (meta.type === 'Launcher Icon')
                     outputFile = path.join(outputPath, config.output.androidDir, 'res', 'drawable-' + meta.resolution, meta.file_name);
                 else outputFile = path.join(outputPath, config.output.androidDir, meta.file_name);
                 assert(fs.statSync(outputFile).isFile());
             }
 
             cb();
-        }); 
+        });
     });
 
-    it('can generate only iOS images and proper output directories', function(cb){
+    it('can generate only iOS images and proper output directories', function (cb) {
         this.timeout(timeout);
-        iconerator.generateIcons(testImg, testImg2, testImg3, outputPath, "ios", null, function(err){
+        iconerator.generateIcons(testImg, testImg2, testImg3, outputPath, "ios", null, null, function (err) {
             assert.ifError(err);
 
             assert(fs.statSync(path.join(outputPath, config.output.iosDir)).isDirectory());
@@ -86,16 +86,16 @@ describe('iconerator', function(){
             assert(!fs.existsSync(path.join(outputPath, config.output.webDir)));
 
             var iconMeta = _.where(config.icons, { platform: 'iOS' });
-            for(var i = 0; i < iconMeta.length; i++){
+            for (var i = 0; i < iconMeta.length; i++) {
                 assert(fs.statSync(path.join(outputPath, config.output.iosDir, iconMeta[i].file_name)).isFile());
             }
             cb();
-        }); 
+        });
     });
 
-    it('can generate only Web images and proper output directories', function(cb){
+    it('can generate only Web images and proper output directories', function (cb) {
         this.timeout(timeout);
-        iconerator.generateIcons(testImg, testImg2, testImg3, outputPath, "web", null, function(err){
+        iconerator.generateIcons(testImg, testImg2, testImg3, outputPath, "web", null, null, function (err) {
             assert.ifError(err);
 
             assert(fs.statSync(path.join(outputPath, config.output.webDir)).isDirectory());
@@ -103,38 +103,38 @@ describe('iconerator', function(){
             assert(!fs.existsSync(path.join(outputPath, config.output.iosDir)));
 
             var iconMeta = _.where(config.icons, { platform: 'Web' });
-            for(var i = 0; i < iconMeta.length; i++){
+            for (var i = 0; i < iconMeta.length; i++) {
                 assert(fs.statSync(path.join(outputPath, config.output.webDir, iconMeta[i].file_name)).isFile());
             }
             cb();
-        }); 
+        });
     });
 
-    it('can generate only Android images and proper output directories', function(cb){
+    it('can generate only Android images and proper output directories', function (cb) {
         this.timeout(timeout);
-        iconerator.generateIcons(testImg, testImg2, testImg3, outputPath, "android", null, function(err){
+        iconerator.generateIcons(testImg, testImg2, testImg3, outputPath, "android", null, null, function (err) {
             assert.ifError(err);
 
             assert(!fs.existsSync(path.join(outputPath, config.output.webDir)));
             assert(!fs.existsSync(path.join(outputPath, config.output.iosDir)));
 
             var androidSubDirs = ['', '/res', '/res/drawable-ldpi', '/res/drawable-mdpi', '/res/drawable-hdpi', '/res/drawable-xhdpi', '/res/drawable-xxhdpi', '/res/drawable-xxxhdpi'];
-            for(var i = 0; i < androidSubDirs.length; i++){
+            for (var i = 0; i < androidSubDirs.length; i++) {
                 assert(fs.statSync(path.join(outputPath, config.output.androidDir, androidSubDirs[i])).isDirectory());
             }
 
             var iconMeta = _.where(config.icons, { platform: 'Android' });
-            for(var i = 0; i < iconMeta.length; i++){
+            for (var i = 0; i < iconMeta.length; i++) {
                 var meta = iconMeta[i];
                 var outputFile;
-                if(meta.type === 'Launcher Icon') 
+                if (meta.type === 'Launcher Icon')
                     outputFile = path.join(outputPath, config.output.androidDir, 'res', 'drawable-' + meta.resolution, meta.file_name);
                 else outputFile = path.join(outputPath, config.output.androidDir, meta.file_name);
                 assert(fs.statSync(outputFile).isFile());
             }
 
             cb();
-        }); 
+        });
     });
 
 });
